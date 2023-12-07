ca974610fb9cbda4b331642ef23f6298301b6c2f
kwongwangjae <121513336+kwongwangjae@users.noreply.github.com>
2023-12-07 22:57:04 +0900

    [level unrated] Title: 정수 찾기, Time: 0.02 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] num_list, int n) {
+        int answer = 0;
+        for(int x : num_list){
+            if(x == n){
+                answer = 1;
+            }
+        }
+        return answer;
+    }
+}
