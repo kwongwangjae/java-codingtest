91e24194278ace8925b8f85e3ed0174c732fa61d
kwongwangjae <121513336+kwongwangjae@users.noreply.github.com>
2023-12-16 23:37:24 +0900

    [level unrated] Title: 홀수 vs 짝수, Time: 0.02 ms, Memory: 67.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int j = 0;
+        int h = 0;
+        for(int i = 0; i< num_list.length; i++){
+            if(i % 2 == 0){
+                j += num_list[i];
+            } else {
+                h += num_list[i];
+            }
+        }
+        if(j >= h){
+            answer = j;
+        } else{
+            answer = h;
+        }
+        return answer;
+    }
+}
