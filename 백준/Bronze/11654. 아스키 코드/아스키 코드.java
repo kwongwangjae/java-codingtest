21c0c8577d234117514ea1d2d12c1e00697658ae
kwongwangjae <121513336+kwongwangjae@users.noreply.github.com>
2024-02-28 22:40:13 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17596 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Main {
+
+	public int solution(char c) {
+		return c;
+	}
+
+	public static void main(String[] args) {
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		// String str = kb.nextLine();
+		char c = kb.next().charAt(0);
+		// int k = kb.nextInt();
+		// int[][] nums = new int[k][2];
+		// for(int i = 0; i < k; i++){
+		// 	nums[i][0] = kb.nextInt();
+		// 	nums[i][1] = kb.nextInt();
+		// }
+
+
+		System.out.println(T.solution(c));
+	}
+}
