76d2886673e5b1a91653fbcd2279c025c2b44ee1
kwongwangjae <121513336+kwongwangjae@users.noreply.github.com>
2024-03-06 23:49:17 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 240 ms, Memory: 18688 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -0,0 +1,36 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+	public static String solution(int N, int K ) {
+		StringBuilder answer = new StringBuilder("<");
+		Deque<Integer> dq = new ArrayDeque<>();
+		for (int i = 1; i <= N; i++) {
+			dq.addLast(i);
+		}
+
+		int num = K;
+		while (!dq.isEmpty()) {
+			for (int i = 0; i < K - 1; i++) {
+				dq.addLast(dq.pollFirst());
+			}
+			answer.append(dq.pollFirst()).append(", ");
+		}
+		// 마지막 쉼표와 공백 제거
+		answer.setLength(answer.length() - 2);
+		answer.append(">");
+		return answer.toString();
+	}
+
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		int N = kb.nextInt();
+		int K = kb.nextInt();
+		System.out.println(solution(N, K));
+	}
+
+}
