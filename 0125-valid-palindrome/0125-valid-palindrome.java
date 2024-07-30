2f90de4b9d718582a17aa57a02571d990ba4d5c1
kwongwangjae <121513336+kwongwangjae@users.noreply.github.com>
2024-07-30 14:19:05 +0900

    Time: 2 ms (99.04%), Space: 42.9 MB (72.65%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,21 +1,42 @@
+// class Solution {
+//     public boolean isPalindrome(String s) {
+//         int start = 0;
+//         int end = s.length() - 1;
+//         while(start < end){
+//             if(!Character.isLetterOrDight(s.charAt(start))){
+//                 start++;
+//             } else if(!Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))){
+//                 retrun false;
+//             }
+//             start++;
+//             end--;
+//         }
+//     }
+//     return true;
+// }
+
+
+
+
+
 class Solution {
     public boolean isPalindrome(String s) {
         int start = 0;
-        int end = s.length() - 1;
+        int end = s.length() -1;
         while(start < end){
             if(!Character.isLetterOrDigit(s.charAt(start))){
                 start++;
             } else if(!Character.isLetterOrDigit(s.charAt(end))){
                 end--;
-            }
-             else{
+            } else{
                 if(Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))){
-                return false;
-             }
-            start++;
-            end--;
+                    return false;
+                }else{
+                    start++;
+                    end--;
+                }
             }
-        }
+        } 
         return true;
     }
 }
