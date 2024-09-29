10996f0159314c47165dc0776c9024f92b831171
kwongwangjae <121513336+kwongwangjae@users.noreply.github.com>
2024-09-30 04:34:58 +0900

    Time: 14 ms (99.54%), Space: 49.6 MB (57.70%) - LeetHub

---
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -0,0 +1,91 @@
+class MyHashMap {
+
+	static class Node{
+		int key, val;
+		Node next;
+
+		Node(int key, int val) {
+			this.key = key;
+			this.val = val;
+		}
+	}
+
+	final Node[] nodes = new Node[100000];
+
+	public MyHashMap() {
+
+	}
+
+	public void put(int key, int value) {
+		int index = key % nodes.length;
+		if (nodes[index] == null) {
+			nodes[index] = new Node(key, value);
+			return;
+		}
+
+		Node node = nodes[index];
+		while (node != null) {
+			if (node.key == key) {
+				node.val = value;
+				return;
+			}
+			if (node.next == null) {
+				break;
+			}
+			node = node.next;
+		}
+        			node.next = new Node(key, value);
+
+	}
+
+	public int get(int key) {
+		int index = key % nodes.length;
+		if (nodes[index] == null) {
+			return -1;
+		}
+		Node node = nodes[index];
+		while (node != null) {
+			if (node.key == key) {
+				return node.val;
+			}
+			node = node.next;
+
+		}
+		return -1;
+
+	}
+
+	public void remove(int key) {
+		int index = key % nodes.length;
+		if (nodes[index] == null) {
+			return;
+		}
+
+		Node node = nodes[index];
+		if (node.key == key) {
+			if (node.next == null) {
+				nodes[index] = null;
+			}else{
+				nodes[index] = node.next;
+			}
+		}
+		Node prev = node;
+		while (node != null) {
+			if (node.key == key) {
+				prev.next = node.next;
+				return;
+			}
+			prev = node;
+			node = node.next;
+		}
+
+	}
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
