7469ec39c864fab4e66004c630ff2658b8d4bd73
kwongwangjae <121513336+kwongwangjae@users.noreply.github.com>
2024-10-05 01:28:05 +0900

    Time: 51 ms (6.78%), Space: 92.1 MB (94.56%) - LeetHub

---
+++ 0077-combinations/0077-combinations.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Solution {
+	public void dfs(List<List<Integer>> results, LinkedList<Integer> elements,  int n, int start, int k) {
+		if (k == 0) {
+			results.add(elements.stream().collect(Collectors.toList()));
+			return;
+		}
+
+
+		for (int i = start; i <= n; i++) {
+			elements.add(i);
+			dfs(results, elements, n, i + 1, k - 1);
+			elements.removeLast();
+		}
+	}
+
+	public List<List<Integer>> combine(int n, int k) {
+		List<List<Integer>> results = new ArrayList<>();
+		dfs(results, new LinkedList<>(),  n, 1, k);
+		return results;
+	}
+}
