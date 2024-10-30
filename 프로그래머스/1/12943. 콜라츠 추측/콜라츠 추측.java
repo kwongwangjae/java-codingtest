a68b67df7c28fb6dc4e39b0cf81981112d7129d2
kwongwangjae <121513336+kwongwangjae@users.noreply.github.com>
2024-10-30 15:18:13 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.10 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,18 +1,18 @@
 class Solution {
-    public int solution(long num) {
-        int answer = 0;
-        while (num != 1) {
-            if (num % 2 == 0) {
-                num /= 2;
-            } else {
-                num = num * 3 + 1;
-            }
-            answer++;
-            if (answer == 500) return -1;
-        }
-        return answer;
-    }
+	public int solution(int num) {
+		if (num == 1) return 0;
+
+		int answer = 0;
+        long n = num;
+
+		while (n != 1 && answer < 500) {
+			if (n % 2 == 0) n = n/2;
+			else n = n * 3 + 1;
+			answer++;
+		}
+
+		if (answer == 500) return -1;
+
+		return answer;
+	}
 }
-
-
-
