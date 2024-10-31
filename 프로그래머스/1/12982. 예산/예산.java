3f417ed0fef4c0fad39d72b1ef5d00e290e0f5fe
kwongwangjae <121513336+kwongwangjae@users.noreply.github.com>
2024-10-31 12:40:20 +0900

    [level 1] Title: 예산, Time: 0.66 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -1,18 +1,17 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
-    public int solution(int[] d, int budget) {
-        int answer = 0;
-        Arrays.sort(d);
-        for(int i = 0; i < d.length; i++){
-            if(d[i] <= budget){
-                budget -= d[i];
-                answer++;
-            } else{
-                break;
-            }
-        }
-        return answer;
-    }
-}
+	public int solution(int[] d, int budget) {
+		int answer = 0;
+		Arrays.sort(d);
 
+		for (int i : d) {
+			if (budget >= i) {
+				answer++;
+				budget -= i;
+			}
+			else break;
+		}
+		return answer;
+	}
+}
