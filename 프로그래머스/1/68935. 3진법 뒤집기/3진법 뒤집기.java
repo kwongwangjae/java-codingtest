874c44eb42fe7bb1b9058c51b8a1b9c91e3c8225
kwongwangjae <121513336+kwongwangjae@users.noreply.github.com>
2024-10-31 12:54:48 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.10 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,13 +1,22 @@
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 class Solution {
-    public int solution(int n) {
-        String str = Integer.toString(n, 3);
-        String rev = "";
-        for(int i = str.length() - 1; i >= 0; i--){
-            rev += str.charAt(i);
-        }
-        
-        return Integer.parseInt(rev, 3);
-    }
+	public int solution(int n) {
+		int answer = 0;
+		Deque<Integer> stack = new ArrayDeque<>();
+
+		while (n > 0) {
+			stack.add(n % 3);
+			n /= 3;
+		}
+
+		int x = 0;
+		while (!stack.isEmpty()) {
+			answer += stack.pollLast() * Math.pow(3, x);
+			x++;
+		}
+
+		return answer;
+	}
 }
